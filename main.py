00320c8b941149d20478065c025fe45de97fd907
ArminParsaDAATLABNij <hipm@yaho.com>
2022-05-05 19:40:40 +0200

    Page Identitty Verification added

--- main.py
+++ main.py
@@ -17,29 +17,20 @@ import json
 import pandas_datareader.data as pdr
 import plotly.offline as py
 import plotly.graph_objs as go
-
-
-
-
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
 
-
-#https://pro-api.coinmarketcap.com/v1/global-metrics/quotes/historical
+# https://pro-api.coinmarketcap.com/v1/global-metrics/quotes/historical
 # List of all blokchain network
-#https://api.coingecko.com/api/v3/asset_platforms
-#https://api.coingecko.com/api/v3/search?query=bitcoin  info about coin
-#Trending coin https://api.coingecko.com/api/v3/search/trending
-#https://api.coingecko.com/api/v3/companies/public_treasury/bitcoin companys btc hold
+# https://api.coingecko.com/api/v3/asset_platforms
+# https://api.coingecko.com/api/v3/search?query=bitcoin  info about coin
+# Trending coin https://api.coingecko.com/api/v3/search/trending
+# https://api.coingecko.com/api/v3/companies/public_treasury/bitcoin companys btc hold
 
-#chart info 1H
-#https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=50
-
-
-
-
-
+# chart info 1H
+# https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=50
 
 
 @app.route('/')
@@ -58,8 +49,8 @@ def homePage():
         elif coin.get('symbol') == 'ETH':
             curentRateEth = float(coin.get('rateUsd'))
             cryptoIdEth = coin.get('id')
-            cryptoSymbolEth  = coin.get('symbol')
-            print(cryptoSymbolEth,cryptoIdEth,curentRateEth)
+            cryptoSymbolEth = coin.get('symbol')
+            print(cryptoSymbolEth, cryptoIdEth, curentRateEth)
         elif coin.get('symbol') == 'USDT':
             curentRateUsdt = float(coin.get('rateUsd'))
             cryptoIdUsdt = coin.get('id')
@@ -68,7 +59,6 @@ def homePage():
             curentRateBnb = float(coin.get('rateUsd'))
             cryptoIdBnbt = coin.get('id')
             cryptoSymbolBnb = coin.get('symbol')
-
 
     df_line = pd.DataFrame([[each_crypto_id.upper(), datetime.utcnow(), current_rate, cryotSymbol],
                             [cryptoIdEth.upper(), datetime.utcnow(), curentRateEth, cryptoSymbolEth],
@@ -80,12 +70,16 @@ def homePage():
 
     print(df_line)
 
-    return render_template("index.html", content=current_rate,btc=each_crypto_id.upper(),price=current_rate,symbol=cryotSymbol,ethName=cryptoSymbolEth,ethSymbol=cryptoIdEth.upper(),ethPrice=curentRateEth,usdtName=cryptoIdUsdt.upper(),usdtSymbol=cryptoSymbolUsdt,usdtPrice=curentRateUsdt
-                           ,bnbName=cryptoIdBnbt.upper(),bnbSymbol=cryptoSymbolBnb,bnbPrice=curentRateBnb
-                           ,time=datetime.utcnow(),info=current_rate,info2=curentRateEth,info3=curentRateUsdt,info4=curentRateBnb)
+    return render_template("index.html", content=current_rate, btc=each_crypto_id.upper(), price=current_rate,
+                           symbol=cryotSymbol, ethName=cryptoSymbolEth, ethSymbol=cryptoIdEth.upper(),
+                           ethPrice=curentRateEth, usdtName=cryptoIdUsdt.upper(), usdtSymbol=cryptoSymbolUsdt,
+                           usdtPrice=curentRateUsdt
+                           , bnbName=cryptoIdBnbt.upper(), bnbSymbol=cryptoSymbolBnb, bnbPrice=curentRateBnb
+                           , time=datetime.utcnow(), info=current_rate, info2=curentRateEth, info3=curentRateUsdt,
+                           info4=curentRateBnb)
 
 
-@app.route('/login/',methods=['POST','GET'])
+@app.route('/login/', methods=['POST', 'GET'])
 def login():
     dataBase = mysql.connector.connect(
         host="localhost",
@@ -93,114 +87,91 @@ def login():
         password="0",
         database="messages"
     )
-    username  = request.form.get('username')
+    username = request.form.get('username')
     message = request.form.get('message')
 
     mycursor = dataBase.cursor()
 
-
     sql = "INSERT INTO messages (name, message) VALUES (%s, %s)"
-    val = (username , message)
+    val = (username, message)
 
     mycursor.execute(sql, val)
     dataBase.commit()
     dataBase.close()
     s = 'Your Message Has Been Sented'
 
+    return render_template("login.html", message=[val, s])
 
-    return render_template("login.html",message=[val,s])
 
-
-@app.route('/btcownchart',methods=['POST','GET'])
+@app.route('/btcownchart', methods=['POST', 'GET'])
 def btcGraph():
-     start = datetime.datetime(2020,1,1)
-     end  = datetime.datetime(2022,4,1)
-     df = pdr.DataReader('BTC-USD','yahoo',start,end)
+    start = datetime.datetime(2020, 1, 1)
+    end = datetime.datetime(2022, 4, 1)
+    df = pdr.DataReader('BTC-USD', 'yahoo', start, end)
 
-     data = [go.Candlestick(x=df.index,
-                            open=df.Open,
-                            high=df.High,
-                            low=df.Low,
-                            close=df.Close)]
+    data = [go.Candlestick(x=df.index,
+                           open=df.Open,
+                           high=df.High,
+                           low=df.Low,
+                           close=df.Close)]
 
-     layout = go.Layout(title='Bicoin CryptoPlanet Chart',
-                        xaxis={'rangeselector': {'buttons': [{'count': 7,
-                                                              'label': '1w',
-                                                              'step': 'day',
-                                                              'stepmode': 'backward'},
-                                                             {'count': 1,
-                                                              'label': '1m',
-                                                              'step': 'month',
-                                                              'stepmode': 'backward'},
-                                                             {'count': 3,
-                                                              'label': '3m',
-                                                              'step': 'month',
-                                                              'stepmode': 'backward'},
-                                                             {'count': 6,
-                                                              'label': '6m',
-                                                              'step': 'month',
-                                                              'stepmode': 'backward'},
-                                                             {'count': 1,
-                                                              'label': '1y',
-                                                              'step': 'year',
-                                                              'stepmode': 'backward'},
-                                                             ]
-                                                 },
-                               'rangeslider': {'visible': True}})
+    layout = go.Layout(title='Bicoin CryptoPlanet Chart',
+                       xaxis={'rangeselector': {'buttons': [{'count': 7,
+                                                             'label': '1w',
+                                                             'step': 'day',
+                                                             'stepmode': 'backward'},
+                                                            {'count': 1,
+                                                             'label': '1m',
+                                                             'step': 'month',
+                                                             'stepmode': 'backward'},
+                                                            {'count': 3,
+                                                             'label': '3m',
+                                                             'step': 'month',
+                                                             'stepmode': 'backward'},
+                                                            {'count': 6,
+                                                             'label': '6m',
+                                                             'step': 'month',
+                                                             'stepmode': 'backward'},
+                                                            {'count': 1,
+                                                             'label': '1y',
+                                                             'step': 'year',
+                                                             'stepmode': 'backward'},
+                                                            ]
+                                                },
+                              'rangeslider': {'visible': True}})
 
-     fig = go.Figure(data=data, layout=layout)
+    fig = go.Figure(data=data, layout=layout)
 
-     fig8 = py.plot(fig)
+    fig8 = py.plot(fig)
 
+    # return tt
 
-     # return tt
-
-     return render_template("bitcoin.html")
-
-
-
-
-
-
-
+    return render_template("bitcoin.html")
 
 
 @app.route('/trend')
 def trending():
     global trendName, trendSymbol, trendId, trendm, trend
-    trendingcoinsInformation = json.loads(requests.get('https://api.coingecko.com/api/v3/search/trending').content).get('coins')
+    trendingcoinsInformation = json.loads(requests.get('https://api.coingecko.com/api/v3/search/trending').content).get(
+        'coins')
 
     for trend in trendingcoinsInformation:
         if trend.get('id') == 'radix':
-
-         print(trend)
+            print(trend)
     df_line2 = pd.DataFrame([[trendm]], columns=['name'])
     print(df_line2)
 
     return render_template("trandingcoins.html", trending=trend)
 
 
-
-
-
-
-
-
 @app.route('/email/', methods=['POST', 'GET'])
 def email1():
-
-
-
-
-
-
-
     return render_template("email.html")
+
 
 @app.route('/tel/', methods=['POST', 'GET'])
 def tel():
     return render_template("tel")
-
 
 
 # @app.route('/header')
@@ -216,25 +187,24 @@ def tel():
 #     return header
 
 
-@app.route('/api/' , methods=['POST', 'GET'])
+@app.route('/api/', methods=['POST', 'GET'])
 def api():
     return render_template("api.html")
 
+
 @app.route('/test')
-#This is going to initialize footer section , or simple way it give to footer values
+# This is going to initialize footer section , or simple way it give to footer values
 def footer_construct():
-    footerinfo =  "https://api.coingecko.com/api/v3/asset_platforms"
+    footerinfo = "https://api.coingecko.com/api/v3/asset_platforms"
     info = requests.get(footerinfo)
     footer = info.text
 
     print(footer)
     return footer
 
+
 @app.route('/test2')
 def test2():
-
-
-
     with open('ids.txt') as json_file:
         data = json.load(json_file)
         store_list = []
@@ -242,7 +212,6 @@ def test2():
             store_details = {"id": None}
 
             store_details['id'] = item['id']
-
 
             store_list.append(store_details)
             store_list2 = store_list
@@ -252,16 +221,13 @@ def test2():
 
             # factom1 = store_list2[0]
 
-
-
-
         # print(store_list2)
-    return render_template("test.html" , test=store_list2 , headerr=storelistvertical)
+    return render_template("test.html", test=store_list2, headerr=storelistvertical)
+
 
 @app.route('/btcchart')
 def btcChart():
     return render_template("bitcoin.html")
-
 
 
 @app.route('/lastadded')
@@ -287,29 +253,29 @@ def lastListingCryptos():
         print(data)
     except (ConnectionError, Timeout, TooManyRedirects) as e:
         print(e)
-    return render_template("lastcoins.html",recentlly=data)
-
+    return render_template("lastcoins.html", recentlly=data)
 
 
 plt.rcParams["figure.figsize"] = [7.50, 3.50]
 plt.rcParams["figure.autolayout"] = True
+
+
 @app.route('/print-plot')
 def plot_png():
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    xs = np.random.rand(100)
+    ys = np.random.rand(100)
+    axis.plot(xs, ys)
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
 
-   fig = Figure()
-   axis = fig.add_subplot(1, 1, 1)
-   xs = np.random.rand(100)
-   ys = np.random.rand(100)
-   axis.plot(xs, ys)
-   output = io.BytesIO()
-   FigureCanvas(fig).print_png(output)
-   return Response(output.getvalue(), mimetype='image/png')
 
 @app.route('/transactions/', methods=['GET'])
 def AnonymouseTransactionsETH():
     '''''at the time just return empty page we fill it up later '''
     return render_template('transaction.html')
-
 
 
 @app.route('/wallet1/', methods=['GET'])
@@ -318,13 +284,10 @@ def wallet1():
     return render_template('wallet.html')
 
 
-
-@app.route('/ercgenerate/', methods=['GET','POST'])
+@app.route('/ercgenerate/', methods=['GET', 'POST'])
 def generateERC20Address():
     global priv, acct
     from web3 import Web3
-
-
 
     infura_url = "https://mainnet.infura.io/v3/7892bc8457dd4b1389ffe11107e7bb08"
     web3 = Web3(Web3.HTTPProvider(infura_url))
@@ -351,12 +314,18 @@ def generateERC20Address():
         acct = Account.from_key(private_key)
         print("Address:", acct.address)
 
-    return render_template("wallet.html", privateKey=priv , Walletaddress=acct.address)
+    return render_template("wallet.html", privateKey=priv, Walletaddress=acct.address)
 
 
-
-
-
+@app.route('/verification/', methods=['GET', 'POST'])
+def identityVerification():
+    app.config["UPLOAD_FOLDER"] = "static/f/"
+    if request.method == 'POST':
+        f = request.files['file']
+        filename = secure_filename(f.filename)
+        f.save(app.config["UPLOAD_FOLDER"] + filename)
+        return render_template("sucsess.html")
+    return render_template("verification.html")
 
 
 #
@@ -405,53 +374,44 @@ def generateERC20Address():
 #
 
 
+# return print(p['id'])
+
+# url1= requests.get('https://api.coingecko.com/api/v3/asset_platforms')
+# load_cdata = json.loads(url1)
 
 
+# f = urllib.urlopen("http://domain/path/jsonPage")
+# values = json.load(f)
+# f.close()
+# print(f)
+# return f
 
-    # return print(p['id'])
+# coininfoinmerhod = list.loads(requests.get('https://api.coingecko.com/api/v3/asset_platforms').content).get('data')
+#
+# converttojson = json.dumps(coininfoinmerhod)
+# print(converttojson)
 
-    # url1= requests.get('https://api.coingecko.com/api/v3/asset_platforms')
-    # load_cdata = json.loads(url1)
-
-
-    # f = urllib.urlopen("http://domain/path/jsonPage")
-    # values = json.load(f)
-    # f.close()
-    # print(f)
-    # return f
-
-    # coininfoinmerhod = list.loads(requests.get('https://api.coingecko.com/api/v3/asset_platforms').content).get('data')
-    #
-    # converttojson = json.dumps(coininfoinmerhod)
-    # print(converttojson)
-
-    # for blokchain in converttojson:
-    #
-    #     if blokchain.get('id') == 'cosmos':
-    #         id2 =blokchain.get('id')
-    #         footerData2 = pd.DataFrame([[id2]],columns=['id'])
-    # print(id2)
-    #
-    # return id2
+# for blokchain in converttojson:
+#
+#     if blokchain.get('id') == 'cosmos':
+#         id2 =blokchain.get('id')
+#         footerData2 = pd.DataFrame([[id2]],columns=['id'])
+# print(id2)
+#
+# return id2
 # @app.route('/save')
 # def saveDataTotext():
 #     f = requests.get('https://api.coingecko.com/api/v3/asset_platforms')
 #     with open('ids.txt', 'w') as fd:
 #         fd.write(f.text)
 
-    # while True:
-    #     global footer
-    #     footerInfoLink = "https://api.coingecko.com/api/v3/asset_platforms"
-    #     footerIno = requests.get(footerInfoLink)
-    #     footer = footerIno.json()
-    #     print(footer)
-    #     return footer
-
-
-
-
-
-
+# while True:
+#     global footer
+#     footerInfoLink = "https://api.coingecko.com/api/v3/asset_platforms"
+#     footerIno = requests.get(footerInfoLink)
+#     footer = footerIno.json()
+#     print(footer)
+#     return footer
 
 
 # def coinmarketCapAPI():
